b7acbbbc3671602b9efd20a51d71d88ab81ff7be
BuildTools <unconfigured@null.spigotmc.org>
2020-01-31 15:56:30 -0500

    progress?

--- gui.py
+++ gui.py
@@ -5,43 +5,33 @@ from temporary import scalex, scaley, screen, screen_size
 
 from astar import astar, heuristic
 
-
-
 print(scalex)
 print(scaley)
 pygame.init()
 
-shape1 = shape()
-shape1.draw((29, 12), (31, 15),(31, 18),
-               (29, 19), (25, 18),(25, 15))
-shape2 = shape()
-shape2.draw((5 , 15),(5 , 19),
-            (17 , 19),(17 , 15))
-shape3 = shape()
-shape3.draw((7 , 3),(10 , 8),
-            (8 , 13),(4 , 12),(3 , 8))
-shape4 = shape()
-shape4.draw((23 , 3),
-            (23 , 11),(28 , 11),(28 , 3))
-shape5 = shape()
-shape5.draw((31 , 3),
-            (34 , 5),(33 , 13),(29 , 5))
-shape6 = shape()
-shape6.draw((20 , 3),
-            (22 , 5),(16 , 8),(16 , 4))
-shape7 = shape()
-shape7.draw((11 , 13),
-            (13 , 7),(15 , 13))
-shape8 = shape()
-shape8.draw((18 , 11),(20 , 16),
-            (23 , 14))
+shape1 = shape((29, 12), (31, 15), (31, 18), (29, 19), (25, 18), (25, 15))
+shape1.draw()
+shape2 = shape((5, 15), (5, 19), (17, 19), (17, 15))
+shape2.draw()
+shape3 = shape((7, 3), (10, 8), (8, 13), (4, 12), (3, 8))
+shape3.draw()
+shape4 = shape((23, 3), (23, 11), (28, 11), (28, 3))
+shape4.draw()
+shape5 = shape((31, 3), (34, 5), (33, 13), (29, 5))
+shape5.draw()
+shape6 = shape((20, 3), (22, 5), (16, 8), (16, 4))
+shape6.draw()
+shape7 = shape((11, 13), (13, 7), (15, 13))
+shape7.draw()
+shape8 = shape((18, 11), (20, 16), (23, 14))
+shape8.draw()
 
 running = True
 
 start = pygame.draw.circle(screen, (0, 255, 0), (3 * scalex, 18 * scaley), screen_size[0] // 100)
 end = pygame.draw.circle(screen, (255, 0, 0), (35 * scalex, 3 * scaley), screen_size[0] // 100)
 
-#path = astar((3 , 18), (35 , 3), heuristic, edges, vertices)
+# path = astar((3 , 18), (35 , 3), heuristic, edges, vertices)
 
 while running:
 
@@ -49,5 +39,5 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    #screen.blit(path)
+    # screen.blit(path)
     pygame.display.update()
--- shape.py
+++ shape.py
@@ -4,13 +4,12 @@ from temporary import screen, scalex, scaley, WHITE
 
 
 class shape:
-    def __init__(self):
+    def __init__(self, *args):
         self.vertices = []
-        self.edges = []
-
-
-    def draw(self, *args):
         self.vertices += [(x * scalex, y * scaley) for x, y in args]
         self.edges = list(zip(self.vertices[:-1], self.vertices[1:])) + [(self.vertices[-1], self.vertices[0])]
+
+
+    def draw(self):
         for i in range(len(self.edges)):
                 pygame.draw.line(screen, WHITE, self.edges[i][0], self.edges[i][1], 3)
