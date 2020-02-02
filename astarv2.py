9daa99dfcb84f14f0ba6068bd3882971c31c4d79
BuildTools <unconfigured@null.spigotmc.org>
2020-02-02 16:59:59 -0500

    Did some stuff, gotta do some more stuff.

--- astarv2.py
+++ astarv2.py
@@ -3,20 +3,17 @@ import pygame
 import math
 import numpy as np
 from collections import defaultdict
-from gui import shapes
 
 
-def best_path(camefrom, current):
-    total_path = {current}
 
-    return total_path
-
-
-def a_star(start, goal, heuristic):
-    openset = [start]
+def a_star(start, goal, heuristic, shapes):
+    total = 0
+    openset = []
+    heapq.heappush(openset, (0, start))
     closedset = []
 
-    camefrom = []
+    camefrom =[]
+    camefrom.append(start)
 
     gscore = defaultdict(lambda: float('inf'))
     gscore[start] = 0
@@ -27,18 +24,42 @@ def a_star(start, goal, heuristic):
 
     while len(openset) > 0:
         current = heapq.heappop(openset)
-        closedset.append(current)
+        total += current[0]
+        camefrom.append(current[1])
+        closedset.append(current[1])
 
         if current == goal:
-            return best_path(camefrom, current)
+            return camefrom
 
+    children = genchildren(current, shapes)
+
+    for child in children:
+        for closed in closedset:
+            if child == closed:
+                continue
+
+        g = cost(current[1], child) + total
+        h = heuristic(current[1], goal)
+        f = g + h
+
+        for open in openset:
+            if child == open[1] and g > open[0]:
+                continue
+
+        heapq.heappush(openset, (f, child))
+        closedset.append(current[1])
     return False
 
 
-def genchildren(current):
+def genchildren(current, shapes):
     possible = []
     for shape in shapes:
-        i = 1
+        for vertex in shape.vertices:
+            for edge in shape.edges:
+                if not dointersect(current[1],vertex,edge[0], edge[1]) and vertex not in possible:
+                    possible.append(vertex)
+                elif vertex == edge[0] or vertex == edge[1] and vertex not in possible:
+                    possible.append(vertex)
     return possible
 
 
@@ -51,7 +72,13 @@ def onsegment(p, q, r):
 
 
 def orientation(p, q, r):
-    val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+    qy = int(q[1])
+    py = int(p[1])
+    rx = int(r[0])
+    qx = int(q[0])
+    px = int(p[0])
+    ry = int(r[1])
+    val = (qy - py) * (rx - qx) - (qx - px) * (ry - qy)
 
     if val == 0:
         return 0
@@ -80,3 +107,11 @@ def dointersect(p1, q1, p2, q2):
         return True
 
     return False
+
+def cost(current, vertex):
+    price = math.sqrt(math.pow((current[0] - vertex[0]), 2) + math.pow((current[1] - vertex[1]), 2))
+    return price
+
+def heuristic(current, goal):
+    h_score = math.sqrt(math.pow((goal[0] - current[0]), 2) + math.pow((goal[1] - current[1]), 2))
+    return h_score
--- gui.py
+++ gui.py
@@ -2,31 +2,27 @@ import pygame
 from shape import shape
 import math
 from temporary import scalex, scaley, screen, screen_size
-from astarv2 import dointersect
-
-from astar import astar, heuristic
+from astarv2 import dointersect, a_star, heuristic
 
 print(scalex)
 print(scaley)
 pygame.init()
-
+GREEN = (0,255,0)
 shapes = [
 shape((29, 12), (31, 15), (31, 18), (29, 19), (25, 18), (25, 15)),
 shape((5, 15), (5, 19), (17, 19), (17, 15)),
-    ]
-'''
 shape((7, 3), (10, 8), (8, 13), (4, 12), (3, 8)),
 shape((23, 3), (23, 11), (28, 11), (28, 3)),
 shape((31, 3), (34, 5), (33, 13), (29, 5)),
 shape((20, 3), (22, 5), (16, 8), (16, 4)),
 shape((11, 13), (13, 7), (15, 13)),
 shape((18, 11), (20, 16), (23, 14))
-'''
+]
 
 shape1 = shapes[0]
 shape2 = shapes[1]
 
-print(dointersect(shape1.vertices[3], shape1.vertices[4], shape2.vertices[2], shape1.vertices[2]))
+#print(dointersect(shape1.vertices[1], shape1.vertices[4], shape2.vertices[2], shape1.vertices[2]))
 for shape in shapes:
     shape.draw()
 
@@ -37,13 +33,15 @@ running = True
 start = pygame.draw.circle(screen, (0, 255, 0), (3 * scalex, 18 * scaley), screen_size[0] // 100)
 end = pygame.draw.circle(screen, (255, 0, 0), (35 * scalex, 3 * scaley), screen_size[0] // 100)
 
-# path = astar((3 , 18), (35 , 3), heuristic, edges, vertices)
+path = a_star((3 * scalex , 18 * scaley), (35 * scalex , 3 * scaley), heuristic, shapes)
 
-while running:
+if path != False:
+    pygame.draw.lines(screen, GREEN, False, path, 3)
+    while running:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    # screen.blit(path)
-    pygame.display.update()
+            # screen.blit(path)
+            pygame.display.update()
